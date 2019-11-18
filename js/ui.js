b8f6f0e24de41564fd142c165e2c9f837abf8367
shah <smuqbilli@gmail.com>
2019-11-19 01:59:20 +0400

    ui changed

--- js/ui.js
+++ js/ui.js
@@ -14,7 +14,7 @@ class UI {
   paint(weather) {
     this.location.textContent = weather.name + ", " + weather.sys.country;
     this.desc.textContent = weather.weather[0].description.toUpperCase();
-    this.string.textContent = `${weather.main.temp} F`;
+    this.string.textContent = `${weather.main.temp} °F`;
     this.icon.setAttribute(
       "src",
       `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
